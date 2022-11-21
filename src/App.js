8ce289add3a3b819df6833f327cb91f532a86cfc
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2022-11-21 15:34:40 -0800

    Firebase setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCIbsZJVSf-tJO2dJzr4_DGNPMApp65ZaQ",
+  authDomain: "superchat-7b816.firebaseapp.com",
+  projectId: "superchat-7b816",
+  storageBucket: "superchat-7b816.appspot.com",
+  messagingSenderId: "319306173968",
+  appId: "1:319306173968:web:9a90868a05daa8fb99c495",
+  measurementId: "G-7EPBB5KFV4",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
